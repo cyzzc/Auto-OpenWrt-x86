f6ad91872672e1e27eeaead89184517a95c9121a
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-05-24 23:02:02 +0800

    Update diy-5.10-part2.sh

--- diy-5.10-part2.sh
+++ diy-5.10-part2.sh
@@ -21,7 +21,7 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 
 ###########添加opentopd主题###########
-#git clone https://github.com/sirpdboy/luci-theme-opentopd package/lean/luci-theme-opentopd
+# git clone https://github.com/sirpdboy/luci-theme-opentopd package/lean/luci-theme-opentopd
 
 ########### 更改默认主题（可选）###########
 # 删除自定义源默认的 argon 主题
